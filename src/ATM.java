06baa3847b0ab836aa01c5ffe0953d5c888b13d4
Alexander Shpin <@lexhpin@gmail.com>
2022-09-20 19:37:39 -0500

    Added the logic for setting up a new account

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,26 @@
 public class ATM {
+    String name;
+    double balance;
 
+    public ATM(String userName, double userBalance) {
+        this.name = userName;
+        this.balance = userBalance;
+    }
+
+    public void withdraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void updateBalance(double amount) {
+        this.balance = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "" + this.name + " has $" + this.balance + " on their account.";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Set up your account: ");
+        System.out.println("Your name: ");
+        String name = String.valueOf(scanner.nextLine());
+
+        System.out.println("Your balance: ");
+        double balance = Double.valueOf(scanner.nextLine());
+
+        ATM account = new ATM(name, balance);
+        System.out.println(account);
 
     }
+
 }
